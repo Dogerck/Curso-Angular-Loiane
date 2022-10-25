fd33e77c61ebe50c4a7bc2b67684dca33b6e1efb
Diego Costa <diegocosta@unip.br>
2022-10-25 18:55:12 -0300

    Criando projeto de forms

---
+++ Forms/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DataFormComponent } from './data-form/data-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
